2216c4a48db0eb0c80e3f908c0c80c434a14072f
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2025-11-12 12:37:47 +0900

    secretsの雛形

---
+++ stampS3R/src/secrets.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID "hogehoge"
+#define WIFI_PASSWORD "hogehoge"
+
+#define AP_SSID "fugafuga"
+#define AP_PASSWORD "fugafuga"
+
+#define HOST_IP "192.168.xxx.xxx"
+#define HOST_OLLAMA_PORT "11434"
